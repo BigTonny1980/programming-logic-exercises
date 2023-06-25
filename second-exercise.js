ee77ad18018c5e9e7b3e1956ee762efdcada775c
Antonio Rodrigues <tonny858895@gmail.com>
2023-06-25 00:22:14 -0300

    Deleting Separate Files

--- second-exercise.js
+++
@@ -1,9 +0,0 @@
-let anyNumber = 3;
-
-if (anyNumber % 2 === 0) {
-   console.log("Even");
-} else {
-   console.log("Odd");
-}
-
-// COMPLETED
--- third-exercise.js
+++
@@ -1,9 +0,0 @@
-let question = window.prompt("How old are you?");
-
-if (question >= 18) {
-   console.log("You can vote");
-} else {
-   console.log("You cannot vote");
-}
-
-// COMPLETED
